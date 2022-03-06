3e9c783fbdada3be06706d3f1e3c6eda6606d246
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-03-06 20:26:36 +0100

    Usage of styleFunctions in Navlink

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import styled from 'styled-components'
 
 const NAV = styled.nav`
@@ -8,13 +8,30 @@ const NAV = styled.nav`
     a {
         margin-right: 16px;
     }
+    /* .active {
+        text-decoration: none;
+        font-weight: bold;
+    } */
 `;
 
+interface navLinkStyles {
+    isActive: boolean
+}
+
 const NavBar = () => {
+
+
+    const navLinkStyles = (props: navLinkStyles) => {
+        return {
+            textDecoration: props.isActive ? 'none' : 'underlined',
+            fontWeight: props.isActive ? 'bold' : 'normal',
+            color: props.isActive ? 'red' : 'green'
+        }
+    }
     return (
         <NAV>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
+            <NavLink to='/' style={navLinkStyles}>Home</NavLink>
+            <NavLink to='/about' style={navLinkStyles}>About</NavLink>
         </NAV>
     )
 }
