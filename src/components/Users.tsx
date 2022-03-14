3f9bfd5140458a8dbdeb995592838e830fe4f39c
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-03-14 20:13:40 +0100

    Usage of searchParams

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 import styled from 'styled-components';
 
 const DIV = styled.div`
@@ -23,12 +23,32 @@ const users = [
 
 
 const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUser = searchParams.get('filter') === 'active'
     return (
         <DIV>
-            {users.map((user) => (
-                <Link to={user.id}>{user.name}</Link>
+            {users.map((user, i) => (
+                <Link key={i} to={user.id}>{user.name}</Link>
             ))}
+
             <Outlet />
+            <div>
+                <button
+                    onClick={() => setSearchParams({ filter: 'active' })}
+                >
+                    Active Users
+                </button>
+                <button
+                    onClick={() => setSearchParams({})}
+                >
+                    Reset filter
+                </button>
+                {
+                    showActiveUser
+                        ? <h2>Showing active user</h2>
+                        : <h2>Showing all user</h2>
+                }
+            </div>
         </DIV>
     )
 }
